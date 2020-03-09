26b6543b685dd87bb879a9f61a8ba78a9cb7976e
Jose Galdamez <galdamez@mac.com>
2020-03-08 20:18:40 -0400

    Thru lesson 19

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {hot} from 'react-hot-loader'
+
+class App extends React.Component {
+    state = {
+        count: 0
+    }
+
+    render() {
+        const { count } = this.state
+        return (
+            <div>
+                <h1>Hello World.</h1>
+                <h2 className={count > 10 ? 'warning' : null}>
+                    Count: {this.state.count}
+                </h2>
+                <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
+                <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+            </div>
+        )
+    }
+}
+
+export default hot(module)(App)
